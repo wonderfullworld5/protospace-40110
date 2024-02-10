eac42ccaa461ec7f6b65fa7bcdc6ef791ef39c3a
wonderfullworld5 <bbb893576@gmail.com>
2024-02-10 15:46:34 +0900

    prototype.html.erbを編集

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_10_021429) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_10_062402) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -43,6 +43,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_10_021429) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "title"
+    t.string "catch_copy"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
