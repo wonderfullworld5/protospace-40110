c49bacbf01861a8cf2785c45b66b2112a8051ef5
wonderfullworld5 <bbb893576@gmail.com>
2024-02-11 07:50:38 +0900

    Commentモデル

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
        has_many :prototypes
-     
+       has_many :comments, dependent: :destroy
        # Include default devise modules. Others available are:
        # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
        devise :database_authenticatable, :registerable,
