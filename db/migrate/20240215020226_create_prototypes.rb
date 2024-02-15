b930229bc544ab3d924c1d9a9871b23ad4e0304b
wonderfullworld5 <bbb893576@gmail.com>
2024-02-16 05:55:11 +0900

    修正

--- db/migrate/20240215020226_create_prototypes.rb
+++ db/migrate/20240215020226_create_prototypes.rb
@@ -1,16 +1,14 @@
 class CreatePrototypes < ActiveRecord::Migration[7.0]
   def change
     create_table :prototypes do |t|
-      t.string :name
+      t.string :name  
       t.text :description
-      t.integer :user_id  # ユーザーのID
-      t.integer :likes_count, default: 0  # いいねの数（デフォルト値は0）
-      t.boolean :published, default: false  # 公開されたかどうかを示すフラグ
-      t.datetime :published_at  # 公開日時
-      t.boolean :featured, default: false  # 特集記事かどうかを示すフラグ
-      t.string :category  # カテゴリー
+      t.integer :user_id
+  
       t.timestamps
     end
   end
 end
+
+
 
