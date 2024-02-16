94dae3c6194abf3a76dda7eeb172d7b1a5d6da69
wonderfullworld5 <bbb893576@gmail.com>
2024-02-16 09:21:57 +0900

    修正

--- db/migrate/20240208214509_add_columns_to_users.rb
+++ db/migrate/20240208214509_add_columns_to_users.rb
@@ -13,17 +13,17 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.0]
       add_column :users, :position, :text
     end
 
-    # プロトタイプのカラムを追加する
-    unless column_exists?(:prototypes, :title)
-      add_column :prototypes, :title, :string, null: false
-    end
+    # プロトタイプのカラムを一時的にコメントアウト
+    # unless column_exists?(:prototypes, :title)
+    #   add_column :prototypes, :title, :string, null: false
+    # end
     
-    unless column_exists?(:prototypes, :catch_copy)
-      add_column :prototypes, :catch_copy, :text, null: false
-    end
+    # unless column_exists?(:prototypes, :catch_copy)
+    #   add_column :prototypes, :catch_copy, :text, null: false
+    # end
     
-    unless column_exists?(:prototypes, :concept)
-      add_column :prototypes, :concept, :text, null: false
-    end
+    # unless column_exists?(:prototypes, :concept)
+    #   add_column :prototypes, :concept, :text, null: false
+    # end
   end
 end
