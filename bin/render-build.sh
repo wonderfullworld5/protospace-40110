11c4eb0a253cd4a263523412836fe0d57d7d95f1
wonderfullworld5 <bbb893576@gmail.com>
2024-02-17 05:58:34 +0900

    修正

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,4 +7,4 @@ bundle exec rake assets:precompile
 bundle exec rake assets:clean
 # bundle exec rake db:migrate
 # ajaxappのデプロイカリキュラムにdbのリセットコマンド
-DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
+#DISABLE_DATABASE_ENVIRONMENT_CHECK=1 bundle exec rake db:migrate:reset
