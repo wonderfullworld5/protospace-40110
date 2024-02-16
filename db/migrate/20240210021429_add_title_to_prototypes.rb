d550ce943de37f38b175985efdfee97766bee5b7
wonderfullworld5 <bbb893576@gmail.com>
2024-02-16 09:58:53 +0900

    修正

--- db/migrate/20240210021429_add_title_to_prototypes.rb
+++ db/migrate/20240210021429_add_title_to_prototypes.rb
@@ -1,5 +1,12 @@
 class AddTitleToPrototypes < ActiveRecord::Migration[7.0]
   def change
-    add_column :prototypes, :title, :string
+    unless table_exists?(:prototypes)
+      create_table :prototypes do |t|
+        
+        t.string :title
+      end
+    else
+      add_column :prototypes, :title, :string unless column_exists?(:prototypes, :title)
+    end
   end
 end
