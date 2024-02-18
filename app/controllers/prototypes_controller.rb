294973083e3423fd3a5fd33ad668956eadbe02d9
wonderfullworld5 <bbb893576@gmail.com>
2024-02-18 11:34:10 +0900

    コード修正

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -49,7 +49,6 @@ class PrototypesController < ApplicationController
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-    
 
   def set_prototype
     @prototype = Prototype.find(params[:id])
