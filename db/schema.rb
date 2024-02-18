7af56d179c4eca6b77da4b35d096dfd337f0a470
wonderfullworld5 <bbb893576@gmail.com>
2024-02-18 14:49:52 +0900

    ローカルでの修正

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_02_18_004328) do
+ActiveRecord::Schema[7.0].define(version: 2024_02_18_054456) do
   create_table "active_storage_attachments", charset: "utf8", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -39,8 +39,20 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_004328) do
     t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
   end
 
+  create_table "comments", charset: "utf8", force: :cascade do |t|
+    t.text "content"
+    t.bigint "prototype_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["prototype_id"], name: "index_comments_on_prototype_id"
+  end
+
   create_table "prototypes", charset: "utf8", force: :cascade do |t|
     t.string "title"
+    t.string "catch_copy"
+    t.string "concept"
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_prototypes_on_user_id"
   end
 
   create_table "users", charset: "utf8", force: :cascade do |t|
@@ -61,4 +73,6 @@ ActiveRecord::Schema[7.0].define(version: 2024_02_18_004328) do
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
   add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "comments", "prototypes"
+  add_foreign_key "prototypes", "users"
 end
