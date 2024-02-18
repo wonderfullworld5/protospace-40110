08ff432b448c72e0f76acd4d3899696931ca88c5
wonderfullworld5 <bbb893576@gmail.com>
2024-02-18 15:06:12 +0900

    マイグレーション削除

--- db/migrate/20240218053820_add_concept_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddConceptToPrototypes < ActiveRecord::Migration[7.0]
-  def change
-    add_column :prototypes, :concept, :string
-  end
-end
--- db/migrate/20240218054034_add_user_id_to_prototypes.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToPrototypes < ActiveRecord::Migration[6.0]
-  def change
-    add_reference :prototypes, :user, foreign_key: true
-  end
-end
