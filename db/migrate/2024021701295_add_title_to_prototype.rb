c9a9603d4717b54aaae135301debdf61010acbd6
wonderfullworld5 <bbb893576@gmail.com>
2024-02-19 11:53:13 +0900

    タイトル修正

--- db/migrate/2024021701295_add_title_to_prototype.rb
+++ db/migrate/2024021701295_add_title_to_prototype.rb
@@ -1,5 +1,5 @@
 class AddTitleToPrototype < ActiveRecord::Migration[7.0]
   def change
-    add_column :prototypes, :title, :string
+    add_column :prototypes, :title, :string, if_not_exists: true
   end
 end
