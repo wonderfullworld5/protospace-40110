d48a07a1e016b4b180c4500c00afb697d9dd9e29
wonderfullworld5 <bbb893576@gmail.com>
2024-02-19 13:31:28 +0900

    マイグレーション変更

--- db/migrate/202402082450_add_columns_to_users.rb
+++ db/migrate/202402082450_add_columns_to_users.rb
@@ -1,18 +1,15 @@
 class AddColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
-    
-    unless column_exists?(:users, :profile)
-      add_column :users, :profile, :text
-    end
-    
-    unless column_exists?(:users, :occupation)
-      add_column :users, :occupation, :text
-    end
-    
-    unless column_exists?(:users, :position)
-      add_column :users, :position, :text
-    end
 
+    create_table :prototypes do |t|
+      t.string :title, null: false
+      t.text :catch_copy, null: false
+      t.text :concept, null: false
+      t.references :user, null: false, foreign_key: true
+      t.timestamps
+    end
+  end
+end
     # プロトタイプのカラムを一時的にコメントアウト
     # unless column_exists?(:prototypes, :title)
     #   add_column :prototypes, :title, :string, null: false
@@ -25,5 +22,4 @@ class AddColumnsToUsers < ActiveRecord::Migration[7.0]
     # unless column_exists?(:prototypes, :concept)
     #   add_column :prototypes, :concept, :text, null: false
     # end
-  end
-end
+  
