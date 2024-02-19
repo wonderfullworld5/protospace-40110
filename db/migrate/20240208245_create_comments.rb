9edb8ded4ef54ceebfaecafefb11ff684bf2fb02
wonderfullworld5 <bbb893576@gmail.com>
2024-02-19 13:58:47 +0900

    マイグレーション修正

--- db/migrate/20240208245_create_comments.rb
+++ db/migrate/20240208245_create_comments.rb
@@ -1,11 +1,9 @@
 class AddColumnsToUsers < ActiveRecord::Migration[7.0]
   def change
-
-    create_table :prototypes do |t|
-      t.string :title, null: false
-      t.text :catch_copy, null: false
-      t.text :concept, null: false
+    create_table :comments do |t|
+      t.text :content, null: false
       t.references :user, null: false, foreign_key: true
+      t.references :prototype, null: false, foreign_key: true
       t.timestamps
     end
   end
