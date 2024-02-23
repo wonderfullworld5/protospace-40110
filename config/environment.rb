58d2f0e901794e5dfd2a99e977d4bcb12994eee4
wonderfullworld5 <bbb893576@gmail.com>
2024-02-23 14:35:12 +0900

    修正

--- config/environment.rb
+++ config/environment.rb
@@ -1,7 +1,7 @@
-Rails.application.configure do
+#Rails.application.configure do
   # Application configuration goes here
-  config.active_storage.service = :local
-end
+  #config.active_storage.service = :local
+#end
 
 # Load the Rails application.
 require_relative "application"
